909897c134d8d8df80eead14c0216da2c0cb0f10
siddhugurubhetti8483 <siddhugurubhetti@gmail.com>
2025-05-26 04:22:25 +0530

    Database created

---
+++ ERMS_Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ERMS_Project.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERMS_Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
